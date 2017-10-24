528c1855e5990ce7160922c138a9c674a2d1b3ac
Gianluca Tursi <gianluca@digitalx.it>
2017-10-24 15:07:49 +0200

    call local bower

--- deploy.cmd
+++ deploy.cmd
@@ -1,13 +1,3 @@
 call npm install
 
-IF EXIST "%DEPLOYMENT_TARGET%\public\bower.json" (
-
-pushd "%DEPLOYMENT_TARGET%\public"
-
 call node_modules\.bin\bower install
-
-IF !ERRORLEVEL! NEQ 0 goto error
-
-popd
-
-)
