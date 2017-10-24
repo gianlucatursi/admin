3ee3abfebbcdbca821e3e507291be477a1e8ddd9
Gianluca Tursi <gianluca@digitalx.it>
2017-10-24 15:07:16 +0200

    try with local bower

--- deploy.cmd
+++ deploy.cmd
@@ -4,7 +4,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\public\bower.json" (
 
 pushd "%DEPLOYMENT_TARGET%\public"
 
-call ..\node_modules\.bin\bower install
+call node_modules\.bin\bower install
 
 IF !ERRORLEVEL! NEQ 0 goto error
 
