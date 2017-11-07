315b7c95c4cac0633e7b4eb7193dade669be46e8
Gianluca Tursi <gianluca@digitalx.it>
2017-11-07 15:32:48 +0100

    added list author_

--- app/features/Details/NewChannel/newchannel.controller.js
+++ app/features/Details/NewChannel/newchannel.controller.js
@@ -12,6 +12,22 @@
     _this.user = AdminService.user;
     _this.city = AdminService.user.citySelected();
     _this.current_state = $state.current;
+    _this.aToAdd = '';
+    _this.new_channel = {
+      authors: []
+    };
+
+    _this.addAuthor = _addAuthor;
+
+    /**
+     * Add author
+     * @param a
+     * @private
+     */
+    function _addAuthor(a){
+      _this.new_channel.authors.push(a);
+      _this.aToAdd = '';
+    }
 
   }
 
