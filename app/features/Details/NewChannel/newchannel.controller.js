a2b4fe61a8fa2c6ace79f238e5d49d86da7af25c
Gianluca Tursi <gianluca@digitalx.it>
2017-11-07 16:03:02 +0100

    fixed detail channel

--- app/features/Details/NewChannel/newchannel.controller.js
+++ app/features/Details/NewChannel/newchannel.controller.js
@@ -14,7 +14,41 @@
     _this.current_state = $state.current;
     _this.aToAdd = '';
     _this.new_channel = {
-      authors: []
+      authors: [],
+      phone : '',
+      website: '',
+      email: '',
+      address: '',
+      isInserzionista: false,
+      orari_specifici: false,
+      days: [ {
+          label: 'Lunedì',
+          flag: false
+        }, {
+          flag: false,
+          label: 'Martedì',
+        },
+        {
+          flag: false,
+          label: 'Mercoledì',
+        },
+        {
+          flag: false,
+          label: 'Giovedì',
+        },
+        {
+          flag: false,
+          label: 'Venerdì',
+        },
+        {
+          flag: false,
+          label: 'Sabato',
+        },
+        {
+          flag: false,
+          label: 'Domenica',
+        }
+      ]
     };
 
     _this.addAuthor = _addAuthor;
@@ -25,6 +59,8 @@
      * @private
      */
     function _addAuthor(a){
+      if(a == '') return;
+
       _this.new_channel.authors.push(a);
       _this.aToAdd = '';
     }
